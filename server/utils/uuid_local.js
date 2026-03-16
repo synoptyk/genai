3818e5d121a0dc0e96535508afe5304d81c715f3
synoptyk <synoptykia@gmail.com>
2026-03-16 17:10:13 -0300

    Update: production sync

---
+++ server/utils/uuid_local.js
@@ -0,0 +1,10 @@
+// Polyfill manual para generar UUIDv4 debido a bloqueo de red en npm
+const crypto = require('crypto');
+
+function v4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+module.exports = { v4 };
