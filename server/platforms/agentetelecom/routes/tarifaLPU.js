5480260acf445cef7324bedaa2085bba37db0671
synoptyk <synoptykia@gmail.com>
2026-03-27 09:58:22 -0300

    fix: import authorize in tarifaLPU.js to resolve crash

--- server/platforms/agentetelecom/routes/tarifaLPU.js
+++ server/platforms/agentetelecom/routes/tarifaLPU.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../../auth/authMiddleware');
+const { protect, authorize } = require('../../auth/authMiddleware');
 const TarifaLPU = require('../models/TarifaLPU');
 
 // =============================================================================
